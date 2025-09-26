2bc35d9a54c08e24e993721e6805ad4b3c08386a
Rebekah-codes <B3cksta@hotmail.co.uk>
2025-09-26 12:53:05 +0100

    Add allauth accounts URLs

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
+
 from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('posts/', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('', views.PostList.as_view(), name='home'),
+    # Add any more specific blog paths here before the catch-all
+    path('posts/<slug:slug>/', views.post_detail, name='post_detail'),  # More specific
+    path('<slug:slug>/', views.post_detail, name='post_detail'),  # Catch-all, must be last
 ]
